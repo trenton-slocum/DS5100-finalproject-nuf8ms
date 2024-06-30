17d511c694e0e13aebf1267ea3164a37309a4003
TrentonPaul_Slocum <trent.slocum@hotmail.com>
2024-06-30 13:53:32 -0400

    FP2

---
+++ montecarlo/montecarlo.py
@@ -0,0 +1,49 @@
+class Die():
+    def __init__(self):
+        pass
+    
+    def initializer():
+        pass
+    
+    def change_side_weight():
+        pass
+    
+    def roll():
+        pass
+    
+    def current_state():
+        pass
+    
+class Game():
+    def __init__(self):
+        pass
+    
+    def initializer():
+        pass
+    
+    def play():
+        pass
+    
+    def last_round():
+        pass
+    
+class Analyzer():
+    def __init__(self):
+        pass
+    
+    def initializer():
+        pass
+    
+    def jackpot():
+        pass
+    
+    def face_count():
+        pass
+    
+    def combo_count():
+        pass
+    
+    def permutation():
+        pass
+    
+    
